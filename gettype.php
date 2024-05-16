dfc0c9d9b6de66501f7374be0cdfec6be50c518a
Lesedi Seema <156032629+Hv3y@users.noreply.github.com>
2024-05-16 23:13:11 +0200

    Update gettype.php

--- gettype.php
+++ gettype.php
@@ -4,7 +4,7 @@ $stnumber=402201272;
 //$is_bool=false;
 //$array=array();
 $stemail="stemail";
-$email="lesediseema43@gmail.com";
+$email="ls@gmail.com";
 print gettype ($number). "<br>";
 print gettype ($stnumber). "<br>";
 print gettype ($email). "<br>";
